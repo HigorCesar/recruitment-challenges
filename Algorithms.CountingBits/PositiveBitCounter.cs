47918fe00b062a69cc3feb55588a216c6bff9b0c
Higor César Ramos <higor.crr@gmail.com>
2018-05-14 16:05:46 +0200

    BitCounter

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -4,6 +4,8 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Linq;
+
 namespace Payvision.CodeChallenge.Algorithms.CountingBits
 {
     using System;
@@ -11,9 +13,25 @@ namespace Payvision.CodeChallenge.Algorithms.CountingBits
 
     public class PositiveBitCounter
     {
+        private char[] ToBinary(int number) => Convert.ToString(number, 2).ToCharArray();
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            if (input < 0)
+                throw new ArgumentException(nameof(input));
+
+            var binaryInput = ToBinary(input);
+            int binaryInputLength = binaryInput.Length;
+            var response = new List<int> { 0 };
+            for (int i = binaryInputLength - 1; i >= 0; i--)
+            {
+                if (binaryInput[i] == '0')
+                    continue;
+
+                response[0]++;
+                response.Add(binaryInputLength - i - 1);
+            }
+
+            return response;
         }
     }
 }
